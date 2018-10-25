9f5f1cfd0c69c5f552cc8b81662d83f604b8da79
LovelyA72 <a72@luotianyi.pro>
2018-10-25 17:52:07 +0800

    啊啊啊啊啊啊

--- public/messageProcessor.php
+++ public/messageProcessor.php
@@ -7,6 +7,7 @@ function isIgnoreGroup($groupID,$ignoreList){
     }
     return false;
 }
+$ignList=Array("788134597");
 if(isIgnoreGroup($Event["group_id"],$ignList)){
     throw new \Exception();
 }
@@ -15,7 +16,6 @@ if(preg_match('/^(['.config('prefix', '!').'])/', $Event['message'], $prefix)){
     if(false===$length)$length=strlen($Event['message']);
     $Command = parseCommand(substr($Event['message'], strlen($prefix[1])-1, $length));
     $Text = substr($Event['message'], $length+2);
-    $ignList=Array("788134597");
     try{
         if($Event["user_id"]==80000000){
             throw new \Exception("请不要使用匿名帐号！");
