e6bf0dc9f1339d97a7c7bc286c5b132829d45be0
LovelyA72 <a72@luotianyi.pro>
2018-10-25 10:14:17 +0800

    we don't really use release

--- module/version/main.php
+++ module/version/main.php
@@ -2,8 +2,6 @@
 
 global $Queue;
 
-$Github = new \Github\Client();
-$result = $Github->api('repo')->releases()->latest('kj415j45', 'kjBot');
-$Queue[]= sendBack("kjBot {$result['tag_name']} {$result['name']}\n项目地址：https://github.com/kj415j45/kjBot\n{$result['body']}");
+$Queue[]= sendBack("YeziiBot v2\nv0.4.1 Adaptable Ame \n项目地址：https://github.com/LovelyA72/YeziiBot-v2\n受AGPL v3开源许可证保护");
 
 ?>
