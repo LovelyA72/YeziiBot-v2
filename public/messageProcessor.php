a7c7cd64994108c3531b11526f90c7888d721145
LovelyA72 <a72@luotianyi.pro>
2018-10-25 17:47:01 +0800

    implement group ignore function

--- public/messageProcessor.php
+++ public/messageProcessor.php
@@ -1,11 +1,22 @@
 <?php
-
+function isIgnoreGroup($groupID,$ignoreList){
+    foreach ($ignoreList as $iid) {
+        if($iid==$groupID){
+            return true;
+        }
+    }
+    return false;
+}
 if(preg_match('/^(['.config('prefix', '!').'])/', $Event['message'], $prefix)){
     $length = strpos($Event['message'], "\r");
     if(false===$length)$length=strlen($Event['message']);
     $Command = parseCommand(substr($Event['message'], strlen($prefix[1])-1, $length));
     $Text = substr($Event['message'], $length+2);
+    $ignList=Array("788134597");
     try{
+        if(isIgnoreGroup($Event["group_id"],$ignList)){
+            throw new \Exception();
+        }
         if($Event["user_id"]==80000000){
             throw new \Exception("请不要使用匿名帐号！");
         };
