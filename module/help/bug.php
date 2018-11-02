240fd22d47d1d139d4133d87aa0532950a98d605
LovelyA72 <a72@luotianyi.pro>
2018-11-02 15:11:15 +0800

    Update module/help/bug.php

--- module/help/bug.php
+++ module/help/bug.php
@@ -2,9 +2,8 @@
 
 global $Queue;
 $msg=<<<EOT
-如果你发现了bug或者希望有新的功能，请在YeziiBot的论坛发帖，让我听到你的声音！\n
-官方：https://qq.acgn.pro/forum\n
-粉丝：https://bbs.yuezhengling.cc/forumdisplay.php?fid=11
+如果你发现了bug或者希望有新的功能，请在YeziiBot的GitHub投送issue，让我听到你的声音！\n
+https://github.com/LovelyA72/YeziiBot-v2
 EOT;
 
 $Queue[]= sendBack($msg);
