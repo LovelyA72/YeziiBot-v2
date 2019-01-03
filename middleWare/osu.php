92b7a1dd2d60b6c4196336fb864ae7ebe07750c5
LovelyA72 <a72@luotianyi.pro>
2019-01-03 11:47:57 +0800

    wrong dict

--- middleWare/osu.php
+++ middleWare/osu.php
@@ -6,7 +6,7 @@ date_default_timezone_set('Asia/Shanghai');
 
 if(preg_match('/see you next time/', $Event['message'])){
     try{
-        if(file_exists("../data/seeya.mp3")){
+        if(file_exists("../storage/data/seeya.mp3")){
             $Queue[]= sendBack(sendRec(getData("seeya.mp3")));
         }
         $CQ->setGroupBan($Event['group_id'], $Event['user_id'], strtotime(((date('H')>=0&&date('H')<=7)?'':'next day').' 7 am')-time());
