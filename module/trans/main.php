e9619ee6d840c1a948c71793b35ec09a8ac078ef
LovelyA72 <a72@luotianyi.pro>
2019-02-13 23:51:37 +0800

    你们让我失望了

--- module/trans/main.php
+++ module/trans/main.php
@@ -1,11 +1,22 @@
 <?php
+
 global $Queue, $Text;
+requireMaster();
+
 use \Statickidz\GoogleTranslate;
 
 $source = nextArg();
 $target = nextArg();
 $trans = new GoogleTranslate();
 
+$nonoWords = "";
+function wordFliter($input,$words){
+    $list = explode(",",words);
+    foreach ($list as $value) {
+        $input = str_replace($value,"*",$input);
+    }
+    return $input;
+}
 //if($source==$target){
 //    throw new \Exception("");
 //}
