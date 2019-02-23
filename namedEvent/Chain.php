d3e3d4099e1f41e24164df48146bca1c55ba125b
LovelyA72 <a72@luotianyi.pro>
2019-02-23 19:08:26 +0800

    Update Chain.php

--- namedEvent/Chain.php
+++ namedEvent/Chain.php
@@ -1,2 +1,14 @@
 <?php
-require("hello.php");
+global $Queue;
+
+//启用的模组应该放在这里，需添加.php后缀
+$procedure = Array(
+    "hello.php"
+);
+
+$prevQueue = sizeof($Queue);
+foreach ($procedure as $file) {
+    if((sizeof($Queue)<=$prevQueue)&&(mb_strlen($file)>0)){
+        require($file);
+    }
+}
