c23ed26551624da5bcafac9d6725106536b79931
LovelyA72 <a72@luotianyi.pro>
2020-04-27 01:37:45 -0400

    [SECURITY PATCH!] patch out calc exploit

--- module/calc/main.php
+++ module/calc/main.php
@@ -1,7 +1,7 @@
 <?php
 
 //-----------------------------------------------------------------------
-//    Copyright (c) 2017-2019 TEAM A72
+//    Copyright (c) 2017-2020 TEAM A72
 //    This file is part of YeziiBot. YeziiBot is distributed with the hope of
 //    attracting more community contributions to the core ecosystem 
 //    of the HeXiaoling Project.
@@ -16,6 +16,9 @@
 //-----------------------------------------------------------------------
 
 global $Queue, $Text;
-$ret = shell_exec(dirname(__FILE__)."/../../native/calc.exe {$Text}");
+
+requireGlobalUserGroup(10);
+$pText = preg_replace("/[^0-9a-zA-Z\+\-\*\/]/", "", $Text );
+$ret = shell_exec(dirname(__FILE__)."/../../native/calc.exe {$pText}");
 
 $Queue[]= sendBack(mb_substr($ret,0,-1));
